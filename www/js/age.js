fbe9adade1b5790290883e816cd743a7e0f0f493
flo mac <flo@flo.com>
2015-09-25 14:35:29 +0200

    age computed automatically

---
+++ www/js/age.js
@@ -0,0 +1,15 @@
+/**
+ * Created by flo on 25/09/15.
+ */
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
+
+document.getElementById('myage').innerHTML = getAge("1991-10-19T08:24:00");
